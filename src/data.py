8f1018e092ef53766cad62f13284271c747ed2a6
charlieberens <charliejb3@gmail.com>
2024-02-13 14:07:50 -0500

    Added GeneticOneHot Transformation. Reworked Genetic Dataset to support Transformations

--- src/data.py
+++ src/data.py
@@ -9,7 +9,8 @@ class GeneticDataset(Dataset):
     Args:
         source (str): The path to the dataset file (csv or tsv).
         sep (str): The separator used in the dataset file. Default is "\t".
-    
+        transform (callable, optional): Optional transforms to be applied to the genetic data. Default is None.
+        
     Returns:
         (genetics, label): A tuple containing the genetic data and the label (phylum, class, order, family, subfamily, tribe, genus, species, subspecies)
     """
@@ -17,12 +18,15 @@ class GeneticDataset(Dataset):
     def __init__(self,
                  source: str,
                  sep: str = "\t",
+                 transform=None
         ):
         try:
             self.data = pd.read_csv(source, sep=sep)
         except FileNotFoundError:
             print(f"File {source} not found.")
             return
+        
+        self.transform = transform
         
     def __len__(self):
         return len(self.data)
@@ -31,5 +35,8 @@ class GeneticDataset(Dataset):
         row = self.data.iloc[idx]
         genetics = row["nucraw"]
         label = [row[c] for c in ["phylum", "class", "order", "family", "subfamily", "tribe", "genus", "species", "subspecies"]]
+
+        if self.transform:
+            genetics = self.transform(genetics)
 
         return genetics, label
---
+++ src/transformations.py
@@ -0,0 +1,47 @@
+import torch
+import torch.nn.functional as F
+
+# Genetics Transformations
+class GeneticOneHot(object):
+    """Map a genetic string to a one-hot encoded tensor, values being in the color channel dimension.
+
+    Args:
+        length (int): The length of the one-hot encoded tensor. Samples will be padded (with Ns) or truncated to this length.
+        zero_encode_unknown (bool, optional): Whether to encode unknown characters as all zeroes. Otherwise, encode them as (1,0,0,0,0). Default is True.
+        include_height_channel (bool, optional): Whether to include a height channel in the one-hot encoding. Default is False.
+    """
+
+    def __init__(self, length:int=720, zero_encode_unknown: bool=True, include_height_channel: bool=False):
+        self.zero_encode_unknown = zero_encode_unknown
+        self.length = length
+        self.include_height_channel = include_height_channel
+
+    def __call__(self, genetic_string: str):
+        """
+        Args:
+            genetics (str): The genetic data to be transformed.
+
+        Returns:
+            torch.Tensor: A one-hot encoded tensor of the genetic data.
+        """
+        # Create a dictionary mapping nucleotides to their one-hot encoding
+        nucleotides = {"N": 0, "A": 1, "C": 2, "G": 3, "T": 4}
+
+        # Convert string to (1, 2, 1, 4, 0, ...)
+        category_tensor = torch.tensor([nucleotides[n] for n in genetic_string])
+        
+        # Pad and crop
+        category_tensor = category_tensor[:self.length]
+        category_tensor = F.pad(category_tensor, (0, self.length - len(category_tensor)), value=0)
+
+        # One-hot encode
+        onehot_tensor = F.one_hot(category_tensor, num_classes=5).permute(1, 0)
+        
+        # Drop the 0th channel, changing N (which is [1,0,0,0,0]) to [0,0,0,0] and making only 4 classes
+        if self.zero_encode_unknown:
+            onehot_tensor = onehot_tensor[1:, :]
+
+        if self.include_height_channel:
+            onehot_tensor = onehot_tensor.unsqueeze(1)
+
+        return onehot_tensor.float()
