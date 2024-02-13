7a19925866c1f376fabeab76c0c857454b4b82de
charlieberens <charliejb3@gmail.com>
2024-02-13 14:26:00 -0500

    Changed models tab size

--- src/models.py
+++ src/models.py
@@ -1,24 +1,23 @@
 import torch.nn as nn
 
-
 def make_cnn(): 
     pass
 
 class MLP(nn.Module):
-  def __init__(self, input_size:int, output_size:int):
-    super().__init__()
-    self.flatten = nn.Flatten()
-    self.linear_relu_stack = nn.Sequential(
-      nn.Linear(input_size, 512),
-      nn.ReLU(),
-      nn.Linear(512, 512),
-      nn.ReLU(),
-      nn.Linear(512, output_size)
-    )
+    def __init__(self, input_size:int, output_size:int):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(input_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, 512),
+            nn.ReLU(),
+            nn.Linear(512, output_size)
+        )
 
-  def forward(self, x):
-    x = self.flatten(x)
-    logits = self.linear_relu_stack(x)
-    return logits
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
 
 
